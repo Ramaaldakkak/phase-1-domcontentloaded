90e7c5215845900be1cc3dc9bab79074ac3076bb
Rama Aldakkak <rama.aldakkak@gmail.com>
2022-06-06 09:40:06 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", () => {
+    let addText = document.querySelector('#text')
+    addText.innerHTML = "This is really cool!"
+    // addText.textContent = "This is really cool!"
+})
